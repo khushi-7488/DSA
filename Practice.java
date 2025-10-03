d51fdf90cae8618a597b1bdafa5185f42fe05c9b
khushi-7488 <khushiitiwari88@gmail.com>
2025-10-03 22:57:00 +0530

    Patters

--- Practice.java
+++ Practice.java
@@ -1,18 +1,13 @@
-import java.util.*;
 
 class Practice {
     public static void main(String args[]) {
-        Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
-        int i = 1;
-        // for(int i=1; i<=n; i++){
+        for (int i = 1; i <= 14; i++) {
+            for (int j = 14; j >= i; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
 
-        // System.out.println("****");
-        // }
-        while (i <= n) {
-            System.out.println("****");
-           i++;
         }
-        scan.close();
+
     }
 }
