ce62fb63fd3b0e9d52a18ce1ac36cd20775d3e9e
khushi-7488 <khushiitiwari88@gmail.com>
2025-10-04 20:36:29 +0530

    function

--- Practice.java
+++ Practice.java
@@ -1,13 +1,69 @@
+import java.util.Scanner;
 
 class Practice {
-    public static void main(String args[]) {
-        for (int i = 1; i <= 14; i++) {
-            for (int j = 14; j >= i; j--) {
+    public static void pattern1() {
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 4; j >= i; j--) {
                 System.out.print("*");
             }
             System.out.println();
 
         }
+    }
 
+    public static void pattern2(int n) {
+        char ch = 'A';
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+
+        }
+    }
+
+    public static void pattern3(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+
+        }
+    }
+
+    public static void pattern4(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
+    public static int calculateSum(int c, int d) {
+        int sum = c + d;
+        System.out.println(sum);
+        return sum;
+    }
+
+    public static void swap(int c, int d) {
+        int temp = c;
+        c = d;
+        d = temp;
+        System.out.println(c);
+        System.out.println(d);
+    }
+
+    public static int product(int c, int d) {
+        int mul = c * d;
+        return mul;
+    }
+
+    public static void main(String args[]) {
+        int a = 5;
+        int b = 10;
+        System.out.println(product(a, b));
     }
 }
