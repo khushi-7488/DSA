a695842a117316688e45b2ca68d79951d6854ef9
khushi-7488 <khushiitiwari88@gmail.com>
2025-10-15 15:28:31 +0530

    java

--- Practice.java
+++ Practice.java
@@ -77,33 +77,33 @@ class Practice {
         return bincoff;
     }
 
-    public static boolean prime(int n) {
-        if (n < 2) {
-            return false;
-        }
+    // public static boolean prime(int n) {
+    //     if (n < 2) {
+    //         return false;
+    //     }
 
-        for (int i = 2; i <= Math.sqrt(n); i++) {
-            if (n % i == 0) {
-                return false;
-            }
-        }
-        return true;
-    }
+    //     for (int i = 2; i <= Math.sqrt(n); i++) {
+    //         if (n % i == 0) {
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
 
-    public static void hollow_rectangle(int totalrows, int totalcolms) {
-        for (int i = 1; i <= totalrows; i++) {
-            for (int j = 1; j <= totalcolms; j++) {
-                if (i == 1 || i == totalrows || j == 1 || j == totalcolms) {
-                    System.out.print("*");
-                } else {
-                    System.out.print("");
+    // public static void hollow_rectangle(int totalrows, int totalcolms) {
+    //     for (int i = 1; i <= totalrows; i++) {
+    //         for (int j = 1; j <= totalcolms; j++) {
+    //             if (i == 1 || i == totalrows || j == 1 || j == totalcolms) {
+    //                 System.out.print("*");
+    //             } else {
+    //                 System.out.print("");
 
-                }
-            }
+    //             }
+    //         }
 
-            System.out.println();
-        }
-    }
+    //         System.out.println();
+    //     }
+    // }
 
 
     public static void main(String args[]) {
