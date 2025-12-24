3e06506b90bcf1f6bcd4ddeddaa908e6e3f124a3
khushi-7488 <khushiitiwari88@gmail.com>
2025-12-24 21:46:08 +0530

    array reverse

--- Array/Arr.java
+++ Array/Arr.java
@@ -1,67 +1,128 @@
 import java.util.*;
 
-public class Arr { 
+public class Arr {
 
-    public static void main(String arhs)
+    // reverse of a array
+    public static void reverse(int num[]) {
+        int first = 0;
+        int last = num.length - 1;
+        while(first  < last) {
+            int temp = num[last];
+            num[last] = num[first];
+            num[first] = temp;
+            first++;
+            last--;
+        }
+        for (int i = 0; i < num.length; i++) {
+            System.out.print(num[i] + " ");
 
-    // find the number 
-    // public static int linearSearch(int numbers[], int key) {
-    //     for (int i = 0; i < numbers.length; i++) {
-    //         if (numbers[i] == key) {
-    //             return i;
-    //         }
+        }
+    }
 
-    //     }
-    //     return -1;
-    // }
+    public static void main(String[] args) {
+        int num[] = { 2, 4, 6, 8, 10 };
+        reverse(num);
+    }
 
-    // public static void main(String[] args) {
-    //     int numbers[] = { 2, 4, 7, 10, 11 };
-    //     int key = 10 ;
-
-    //     int index = linearSearch(numbers, key);
-    //     if (index == -1) {
-    //         System.out.println("not found");
-    //     }else{
-    //         System.out.println(index);
-    //     }
-
-    //     for (int i = 0; i < numbers.length; i++) {
-    //         System.out.println();
-    //     }
-    // }
 }
+// finding a number in binarySearch
+// public static int binarySearch(int num[], int key) {
+// int start = 0;
+// int end = num.length - 1;
 
-    // update a array
-    // public static void update(int marks[]) {
-    // for (int i = 0; i < marks.length; i++) {
-    // marks[i] = marks[i] + 2;
-    // }
-    // }
+// for (int i = start; i <= end; i++) {
+// int mid = (start + end) / 2;
+// if (num[mid] == key) {
+// return mid;
+// }
+// if (num[mid] > key) {
+// end = mid - 1;
+// } else {
+// start = mid + 1;
+// }
+// }
+// return -1;
+// }
 
-    // public static void main(String[] args) {
+// public static void main(String[] args) {
+// int num[] = { 5, 10, 12, 15, 17, 18, 19 };
+// int key = 17;
+// int search = binarySearch(num, key);
+// System.out.println(search);
+// }
+// }
 
-    // int marks[] = { 99, 89, 96 };
-    // update(marks);
-    // for (int i = 0; i < marks.length; i++) {
-    // System.out.println(marks[i]);
-    // }
+// largest number linear search
+// public static int largest(int num[]) {
+// int min = Integer.MIN_VALUE;
+// for (int i = 0; i < num.length; i++) {
+// if (min < num[i]) {
+// min = num[i];
+// }
+// }
+// return min;
+// }
 
-    // basic array
-    // int marks[] = new int[50];
+// public static void main(String args[]) {
+// int num[] = { 1, 2, 3, 5, 7, 4 };
+// System.out.println(largest(num));
 
-    // Scanner sc = new Scanner(System.in);
-    // marks[0] = sc.nextInt();
-    // marks[1] = sc.nextInt();
-    // marks[2] = sc.nextInt();
+// }
+// }
 
-    // System.out.println("phy :" + marks[0]);
-    // System.out.println("chem :" + marks[1]);
-    // System.out.println("math :" + marks[2]);
+// find the number linear search
+// public static int linearSearch(int numbers[], int key) {
+// for (int i = 0; i < numbers.length; i++) {
+// if (numbers[i] == key) {
+// return i;
+// }
 
-    // int percentage = (marks[0]+marks[1]+marks[2])/3;
-    // System.out.println(percentage + "%");
+// }
+// return -1;
+// }
 
-    // System.out.println(marks.length);
+// public static void main(String[] args) {
+// int numbers[] = { 2, 4, 7, 10, 11 };
+// int key = 10 ;
 
+// int index = linearSearch(numbers, key);
+// if (index == -1) {
+// System.out.println("not found");
+// }else{
+// System.out.println(index);
+// }
 
+// }
+// }
+
+// update a array
+// public static void update(int marks[]) {
+// for (int i = 0; i < marks.length; i++) {
+// marks[i] = marks[i] + 2;
+// }
+// }
+
+// public static void main(String[] args) {
+
+// int marks[] = { 99, 89, 96 };
+// update(marks);
+// for (int i = 0; i < marks.length; i++) {
+// System.out.println(marks[i]);
+// }
+
+// basic array
+// int marks[] = new int[50];
+
+// Scanner sc = new Scanner(System.in);
+// marks[0] = sc.nextInt();
+// marks[1] = sc.nextInt();
+// marks[2] = sc.nextInt();
+
+// System.out.println("phy :" + marks[0]);
+// System.out.println("chem :" + marks[1]);
+// System.out.println("math :" + marks[2]);
+
+// int percentage = (marks[0]+marks[1]+marks[2])/3;
+// System.out.println(percentage + "%");
+
+// System.out.println(marks.length);
