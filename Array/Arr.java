8e2d744a5c7eb1ff424246a7a3e5e470fe48eee4
khushi-7488 <khushiitiwari88@gmail.com>
2025-12-24 22:56:35 +0530

    array reverse

--- Array/Arr.java
+++ Array/Arr.java
@@ -2,29 +2,47 @@ import java.util.*;
 
 public class Arr {
 
-    // reverse of a array
-    public static void reverse(int num[]) {
-        int first = 0;
-        int last = num.length - 1;
-        while(first  < last) {
-            int temp = num[last];
-            num[last] = num[first];
-            num[first] = temp;
-            first++;
-            last--;
-        }
+    // pairing the numbers 
+    public static void pairs(int num[]) {
         for (int i = 0; i < num.length; i++) {
-            System.out.print(num[i] + " ");
-
+            for (int j = i + 1; j < num.length; j++) {
+                System.out.print("(" + num[i] + "," + num[j] + ") ");
+            }
+            System.out.println();
         }
     }
 
     public static void main(String[] args) {
-        int num[] = { 2, 4, 6, 8, 10 };
-        reverse(num);
+        int num[] = { 2, 3, 4, 5, 6 };
+        pairs(num);
     }
 
 }
+
+// reverse of a array
+// public static void reverse(int num[]) {
+// int first = 0;
+// int last = num.length - 1;
+// while(first < last) {
+// int temp = num[last];
+// num[last] = num[first];
+// num[first] = temp;
+// first++;
+// last--;
+// }
+// for (int i = 0; i < num.length; i++) {
+// System.out.print(num[i] + " ");
+
+// }
+// }
+
+// public static void main(String[] args) {
+// int num[] = { 2, 4, 6, 8, 10 };
+// reverse(num);
+// }
+
+// }
+
 // finding a number in binarySearch
 // public static int binarySearch(int num[], int key) {
 // int start = 0;
