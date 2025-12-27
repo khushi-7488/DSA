77bded8430c4996d9da8efe8bec85ebe9d6c923f
khushi-7488 <khushiitiwari88@gmail.com>
2025-12-27 19:38:34 +0530

    trapping water

---
+++ Array/TrappingRainwater.java
@@ -0,0 +1,10 @@
+public class TrappingRainwater {
+    public static void trapWater(int height[]) {
+
+    }
+
+    public static void main(String[] args) {
+        int height[] = { 4, 2, 0, 6, 3, 2, 5 };
+        trapWater(height);
+    }
+}
