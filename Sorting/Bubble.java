cc54f096a4a432a27bb7b7af0e9a3049b660d089
khushi-7488 <khushiitiwari88@gmail.com>
2025-12-29 00:31:36 +0530

    bubble sort

---
+++ Sorting/Bubble.java
@@ -0,0 +1,24 @@
+package Sorting;
+
+public class Bubble {
+    public static void BubbleSort(int num[]) {
+        int n = num.length - 1;
+        for (int i = 0; i < num.length; i++) {
+            for (int j = 0; j < n; j++) {
+                if (num[j] > num[j+1]) {
+                    int temp = num[j];
+                    num[j] = num[j+1];
+                    num[j+1] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < num.length; i++) {
+            System.out.println(num[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        int num[] = { 3, 5, 2, 6, 1,4,10,14 };
+        BubbleSort(num);
+    }
+}
