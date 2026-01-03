1e3e71eb89457061bfc47f349234c344c2453b42
khushi-7488 <khushiitiwari88@gmail.com>
2026-01-03 19:32:12 +0530

    operations on bitmanipulation

---
+++ BitManipulation/Operations.java
@@ -0,0 +1,35 @@
+package BitManipulation;
+
+public class Operations {
+    public static int getIthBit(int n, int i) {
+        int bitMask = 1 << i;
+        if ((n & bitMask) == 0) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+
+    public static void setIthBit(int n, int i) {
+        int bitMask = 1 << i;
+        System.out.println(n | bitMask);
+    }
+
+    public static int clearIthBit(int n, int i) {
+        int bitMask = (~0) << i;
+        return n & bitMask;
+    }
+    public static int clearRangeOfBit(int n, int i, int j) {
+        int a = ((~0) << (j+1));
+        int b = (1<<i)-1;
+        int bitMask = a | b;
+        return n & bitMask;
+    }
+
+    public static void main(String[] args) {
+        // System.err.println(getIthBit(5, 3));
+        // System.err.println(clearIthBit(15, 2));
+        System.err.println(clearRangeOfBit(10, 2,4));
+        // setIthBit(15, 2);
+    }
+}
