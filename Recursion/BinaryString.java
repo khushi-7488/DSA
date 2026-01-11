10da554903f91ae5c184e5cc0b9abf77bc484a88
khushi-7488 <khushiitiwari88@gmail.com>
2026-01-11 15:38:19 +0530

    binaryString

---
+++ Recursion/BinaryString.java
@@ -0,0 +1,22 @@
+package Recursion;
+
+public class BinaryString {
+    public static void printStr(int n, String newStr, int lastBit) {
+        if (n == 0) {
+            System.out.println(newStr);
+            return;
+        }
+
+        printStr(n - 1, newStr + "0", 0);
+
+        if (lastBit == 0) {
+            printStr(n - 1, newStr + "1", 1);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        printStr(3, "", 0);
+    }
+} 
+                
