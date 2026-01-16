69d961df9df1b41e8d2fcbe7ba8d66330bbf0233
khushi-7488 <khushiitiwari88@gmail.com>
2026-01-17 00:52:36 +0530

    merge sort and quick sort are completed

--- DivideAndConquer/MergeSort.java
+++ DivideAndConquer/MergeSort.java
@@ -1,15 +1,21 @@
 package DivideAndConquer;
 
 public class MergeSort {
+    public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
     public static void mergeSort(int arr[], int si, int ei) {
 
-        if (si == ei && si >= ei) { // base case
+        if (si >= ei) { // base case
             return;
         }
-        int mid = si * (si - ei) / 2;
+        int mid = si + (ei - si) / 2;
 
-        mergeSort(arr, si, mid);
-        mergeSort(arr, si, mid - 1);
+        mergeSort(arr, si, mid); //left
+        mergeSort(arr, mid + 1, ei);  //right
         merge(arr, si, mid, ei);
 
     }
@@ -17,24 +23,38 @@ public class MergeSort {
     public static void merge(int arr[], int si, int mid, int ei) {
         int temp[] = new int[ei - si + 1];
         int i = si;
-        int j = ei;
+        int j = mid + 1;
         int k = 0;
         while (i <= mid && j <= ei) {
             if (arr[i] < arr[j]) {
                 temp[k] = arr[i];
                 i++;
-                k++;
-            }else{
-              temp[k] = arr[j];
+
+            } else {
+                temp[k] = arr[j];
                 j++;
-                k++;   
+
             }
+            k++;
+        }
+        // left
+        while (i <= mid) {
+            temp[k++] = arr[i++];
+        }
+        // right
+        while (j <= ei) {
+            temp[k++] = arr[j++];
         }
 
+        // copy temp to original arr
+        for (k = 0, i = si; k < temp.length; k++, i++) {
+            arr[i] = temp[k];
+        }
     }
 
     public static void main(String[] args) {
         int arr[] = { 6, 3, 9, 5, 2, 8 };
         mergeSort(arr, 0, arr.length - 1);
+        printArr(arr);
     }
 }
