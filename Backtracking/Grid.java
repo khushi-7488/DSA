f4cb90ff71f5b718fa6b2bb9a14dcc8ac34539e4
khushi-7488 <khushiitiwari88@gmail.com>
2026-01-20 16:12:10 +0530

    grid

---
+++ Backtracking/Grid.java
@@ -0,0 +1,22 @@
+package Backtracking;
+
+public class Grid {
+    public static int grids(int grid[][], int x, int y) {
+        // base case
+        if (x == grid.length - 1 || y == grid.length - 1) {
+           return 1;
+        }else if(x == grid.length || y == grid.length ){
+            return 0;
+        }
+        int w1 = grids(grid, x, y + 1);
+        int w2 = grids(grid, x + 1, y);
+        return w1 + w2;
+    }
+
+    public static void main(String[] args) {
+        int n = 1000;
+        int grid[][] = new int[n][n];
+       System.out.println(grids(grid, 0, 0));
+
+    }
+}
