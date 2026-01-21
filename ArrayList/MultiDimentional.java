5e66f981d1abd302d06fa19045e51c0ba8fecb94
khushi-7488 <khushiitiwari88@gmail.com>
2026-01-21 19:17:26 +0530

    swap and sorting and multidimentional array

---
+++ ArrayList/MultiDimentional.java
@@ -0,0 +1,23 @@
+package ArrayList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MultiDimentional {
+    public static void main(String[] args) {
+        ArrayList<ArrayList<Integer>> mainList = new ArrayList<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        mainList.add(list);
+        for (int i = 0; i < mainList.size(); i++) {
+            ArrayList<Integer> currList = mainList.get(i);
+            for (int j = 0; j < currList.size(); j++) {
+                System.out.print(currList.get(j));
+            }
+            System.out.println();
+        }
+    }
+}
