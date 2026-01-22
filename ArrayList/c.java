60dfb2a4f58c17691297e554174262267e605673
khushi-7488 <khushiitiwari88@gmail.com>
2026-01-22 17:59:39 +0530

    repeation of a array

---
+++ ArrayList/c.java
@@ -0,0 +1,12 @@
+package ArrayList;
+
+public class c {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3 };
+        int arr2 = arr.length * 2;
+
+        for (int i = 0; i < arr2; i++) {
+            System.out.print(arr[i % arr.length]);
+        }
+    }
+}
