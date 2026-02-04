f3e3d09e986637ac16b683546796a2df4dc60842
khushi-7488 <khushiitiwari88@gmail.com>
2026-02-04 23:00:04 +0530

    first non repeating number

--- Queue/FirstNonRepeatingLetter.java
+++ Queue/FirstNonRepeatingLetter.java
@@ -10,13 +10,22 @@ public class FirstNonRepeatingLetter {
 
         for (int i = 0; i < str.length(); i++) {
             char ch = str.charAt(i);
-
             q.add(ch);
-            
+            freq[ch - 'a']++;
+            while (!q.isEmpty() && freq[q.peek() - 'a'] > 1) {
+                q.remove();
+            }
         }
+        if (q.isEmpty()) {
+            System.out.println("-1");
+        } else {
+            System.out.print(q.peek());
+        }
+        System.out.println();
     }
 
     public static void main(String[] args) {
         String str = "aabccxb";
+        printNonoRepeating(str);
     }
 }
