76f2ce4c8e9a5493c15f8ae482eb3664c8161309
khushi-7488 <khushiitiwari88@gmail.com>
2026-02-10 20:35:03 +0530

    MinimumSumAbloluteDiffPairs

--- greedyAlgorithm/MinimumSumAbloluteDiffPairs.java
+++ greedyAlgorithm/MinimumSumAbloluteDiffPairs.java
@@ -1,7 +1,19 @@
 package greedyAlgorithm;
 
+import java.util.Arrays;
+
 public class MinimumSumAbloluteDiffPairs {
     public static void main(String[] args) {
-        
+        int a[] = { 1, 7, 5 };
+        int b[] = { 2, 3, 5 };
+
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        int minDiff = 0;
+        for (int i = 0; i < a.length; i++) {
+            minDiff += Math.abs(a[i] - b[i]);
+        }
+        System.out.println(minDiff);
     }
 }
