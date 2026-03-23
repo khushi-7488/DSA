ea7885b720371a7162db08b3d1da0fa43b3d75d1
khushi-7488 <khushiitiwari88@gmail.com>
2026-03-23 23:50:53 +0530

    counting  sort

---
+++ SortingPrac/Counting.java
@@ -0,0 +1,30 @@
+package SortingPrac;
+
+public class Counting {
+    public static void sorting(int arr[]) {
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            max = Math.max(max, arr[i]);
+        }
+        int count[] = new int[max + 1];
+        for (int i = 0; i < arr.length; i++) {
+            count[arr[i]]++;
+        }
+        int j = 0;
+        for (int i = 0; i < count.length; i++) {
+            while (count[i] > 0) {
+                arr[j] = i;
+                j++;
+                count[i]--;
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 5, 4, 5, 3, 7, };
+        sorting(arr);
+    }
+}
